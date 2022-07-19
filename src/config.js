88dd6082e3f321a370eafec7b8c382489204da4c
lixin-940207 <alix.xinli@gmail.com>
2022-07-19 15:40:01 +0200

    use env for API hosts

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
+const news_spider_api_host = process.env.NEWS_SPIDER_API_HOST || "127.0.0.1";
+const news_spider_api_port = process.env.NEWS_SPIDER_API_PORT || 5000;
+
 module.exports = {
     APIs: {
-        'FranceNewsAPI': 'http://127.0.0.1:5000/latest_france_news',
-        'WorldNewsAPI': 'http://127.0.0.1:5000/latest_world_news',
-        'ChinaNewsAPI': 'http://127.0.0.1:5000/latest_china_news',
-        'BBCAPI': 'http://127.0.0.1:5000/bbc',
-        'LeMondeAPI': 'http://127.0.0.1:5000/lemonde',
-        'NYTimesAPI': 'http://127.0.0.1:5000/nytimes',
-        'LeFigaroAPI': 'http://127.0.0.1:5000/lefigaro',
+        'FranceNewsAPI': `http://${news_spider_api_host}:${news_spider_api_port}/latest_france_news`,
+        'WorldNewsAPI': `http://${news_spider_api_host}:${news_spider_api_port}/latest_world_news`,
+        'ChinaNewsAPI': `http://${news_spider_api_host}:${news_spider_api_port}/latest_china_news`,
+        'BBCAPI': `http://${news_spider_api_host}:${news_spider_api_port}/bbc`,
+        'LeMondeAPI': `http://${news_spider_api_host}:${news_spider_api_port}/lemonde`,
+        'NYTimesAPI': `http://${news_spider_api_host}:${news_spider_api_port}/nytimes`,
+        'LeFigaroAPI': `http://${news_spider_api_host}:${news_spider_api_port}/lefigaro`,
     }
 }
